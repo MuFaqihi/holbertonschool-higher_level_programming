39033c3f4d72b8ab2212a4cbf65c14fadffe1e9f
MuFaqihi <mmnouc2@gmail.com>
2025-05-17 17:01:38 +0000

    a function that adds 2 integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Module for add_integer method."""
 
-
 def add_integer(a, b=98):
     """Adds two integers.
 
@@ -22,8 +21,7 @@ def add_integer(a, b=98):
         raise TypeError('b must be an integer')
     return int(a) + int(b)
 
+
 if __name__ == "__main__":
-
-
     import doctest
     doctest.testfile("tests/0-add_integer.txt")
