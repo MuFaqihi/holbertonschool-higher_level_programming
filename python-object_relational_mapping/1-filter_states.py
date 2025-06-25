ade840b1203f72753f1277ac583a22ee7a0360e6
MuFaqihi <mmnouc2@gmail.com>
2025-06-25 10:03:05 +0000

    fix

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,27 +1,15 @@
 #!/usr/bin/python3
-"""
-répertorie tous les états dont le nom commence par N (N majuscule)
-de la base de données hbtn_0e_0_usa
-
-"""
+"""Lists all states starting with 'N' from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    # Connect to MySQL server
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
-    # Create a cursor object
     cursor = db.cursor()
-    # Execute the query
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    # Fetch all the rows
-    rows = cursor.fetchall()
-    # Print the results
-    for row in rows:
-        if row[1][0] == 'N':
-            print(row)
-    # Close the cursor and database connection
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    for row in cursor.fetchall():
+        print(row)
     cursor.close()
     db.close()
