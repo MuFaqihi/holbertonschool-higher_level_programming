e87a7833dafd5fc660e2c251b84925b7764a477d
MuFaqihi <mmnouc2@gmail.com>
2025-06-26 09:27:16 +0000

    fix

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
- Python file similar to model_state.py named model_city.py 
-that contains the class definition of a City
+ Python file similar to model_state.py named model_city.py
 """
 
 from sqlalchemy import Column, ForeignKey, Integer, String
